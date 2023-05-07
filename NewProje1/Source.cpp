ceddb2668c40b045c7b7db412b3c847360dcaa06
Eliyev Eli Sohret <aliyev_ali@gmail.ru>
2023-05-07 09:31:44 +0400

    My Test 1

---
+++ NewProje1/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+void main() {
+	int num1, num2;
+	cout << "Enter of num1 : ";
+	cin >> num1;
+	cout << "Enter of num2 : ";
+	cin >> num2;
+
+	int sum = num1 + num2;
+	cout << "Total : " << sum;
+
+}
